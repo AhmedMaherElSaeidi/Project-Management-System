375d25c5917d1d856c42b404e1f3c6b62e02a9b4
Ahmed Maher <ahmedelseaidy22@gmail.com>
2023-06-23 21:58:51 +0200

    first commit

---
+++ src/ProjectManagementSystemProject/ProjectManagementSystemProject.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package ProjectManagementSystemProject;
+
+public class ProjectManagementSystemProject {
+
+    public static String storagePath = "FileBase/";
+
+    public static void main(String[] args) {
+        LogIn login = new LogIn();
+        login.show();
+    }
+}
